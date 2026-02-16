e1bc072e8ed14ee917bc6a58062bbf2c1fc5693f
DanielConduri <jonathand.tene@espoch.edu.ec>
2026-02-16 16:23:36 -0500

    update

--- marriage/src/components/ui/calendar.tsx
+++ marriage/src/components/ui/calendar.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import * as React from 'react'
-import { ChevronLeft, ChevronRight } from 'lucide-react'
+import { ChevronDown, ChevronLeft, ChevronRight, ChevronUp } from 'lucide-react'
 import { DayPicker } from 'react-day-picker'
 
 import { cn } from '@/lib/utils'
@@ -54,8 +54,19 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        Chevron: ({ orientation, className }) => {
+          if (orientation === 'left') {
+            return <ChevronLeft className={cn('h-4 w-4', className)} />
+          }
+          if (orientation === 'down') {
+            return <ChevronDown className={cn('h-4 w-4', className)} />
+          }
+          if (orientation === 'up') {
+            return <ChevronUp className={cn('h-4 w-4', className)} />
+          }
+
+          return <ChevronRight className={cn('h-4 w-4', className)} />
+        },
       }}
       {...props}
     />
